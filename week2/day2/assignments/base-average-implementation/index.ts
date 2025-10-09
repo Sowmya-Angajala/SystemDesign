769849647369b65d1b75567b13ce670fa77a4651
Sowmya-Angajala <angajalasowmya02@gmail.com>
2025-10-10 00:40:22 +0530

    Base Beverage Implementation is done

---
+++ week2/day2/assignments/base-average-implementation/index.ts
@@ -0,0 +1,20 @@
+abstract class Beverage {
+    abstract getDescription(): string;
+    abstract getCost(): number;
+}
+
+// GreenTea class extending Beverage
+class GreenTea extends Beverage {
+    getDescription(): string {
+        return "Green Tea";
+    }
+
+    getCost(): number {
+        return 40;
+    }
+}
+
+// Test
+const tea = new GreenTea();
+console.log(tea.getDescription()); // Green Tea
+console.log(tea.getCost());        // 40
